d7762e9ea58d2e002192f717d7938f0af69bd7ec
Deepak <ds954642@gmail.com>
2026-04-04 06:47:18 +0530

    add frontend env var

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://gonative-render.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
